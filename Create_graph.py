fe33fdc9da9d0ec93a165f73afedec39d1ce3836
Hashini Kaweesha Ranaweera <129282753+RanaweeraHK@users.noreply.github.com>
2024-01-20 00:45:47 +0530

    Create Create_graph.py

---
+++ Create_graph.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4],[4,5,7,8])
+# graph1 = plt.plot([x coordination],[y coordination])
+
+plt.xlabel("Number of students")
+plt.ylabel("Average Height")
+plt.title("Height graph")
+plt.show()
